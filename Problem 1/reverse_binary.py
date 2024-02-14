3b25a7edf888d36639b430b01384df05ae662907
rileyspollard <rileyspollard@outlook.com>
2024-02-14 14:21:56 -0600

    Problem 1 and 2

--- Problem 1/reverse_binary.py
+++ Problem 1/reverse_binary.py
@@ -1,8 +1,8 @@
 """
 Complete the following python code to print the reverse binary representation of positive integer number entered by the user.
 
-Name:
-Lab Time:
+Name: Riley Pollard
+Lab Time: Thursday 2pm lab, Wednesday class
 
 """
 
@@ -10,6 +10,20 @@ Lab Time:
 def reverse_binary():
     user_num = int(input())
     # YOUR CODE HERE
+    binary_val = ''
+#write your while loop here
+    while user_num > 0:
+        #write your code
+        placement = user_num %2
+        binary_val += str(placement)
+        user_num = user_num // 2
+        
+
+
+    print(binary_val)
 
 if __name__ == "__main__":
     reverse_binary()
+
+
+    
--- Problem 2/password_mod.py
+++ Problem 2/password_mod.py
@@ -9,6 +9,22 @@ def password_mod():
     word = input()
     password = ''
     # Type your code here.
+    for i in word:
+        if i == 'i':
+            password+='1'
+        elif i == 'a':
+            password+='@'
+        elif i == 'm':
+            password+='M'
+        elif i == 'B':
+            password+='8'
+        elif i == 's':
+            password+='$'
+        else:
+            password += i
+        
+    password += '!'
+    print(password)
 
 if __name__ == "__main__":
     password_mod()
