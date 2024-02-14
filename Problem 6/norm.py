c609de155c0e63a1d5e2fd44690d83e575f63f45
rileyspollard <rileyspollard@outlook.com>
2024-02-14 17:50:25 -0600

    All complete

--- Problem 6/norm.py
+++ Problem 6/norm.py
@@ -7,7 +7,19 @@ Lab Time: Thursday 2pm lab, wednesday class
 
 def norm():
     # Write your code here
-    print('hello')
+    number_floats = int(input())
+    numarr = {}
+    highnum = float(0)
+    for x in range (0,number_floats):
+        numarr[x] = float(input())
+    
+    highnum = numarr[0]
+    for x in range (1,number_floats):
+        if highnum < numarr[x]:
+            highnum = numarr[x]
+    for x in range (0,number_floats):
+        numarr[x] /= highnum 
+        print(f'{numarr[x]:.2f}')
 
 if __name__ == "__main__":
     norm()
